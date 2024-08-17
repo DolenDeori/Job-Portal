8984ba1e517c5998baa8f5c059fcc586aa46af56
DolenDeori <dulench.deori565@gmail.com>
2024-08-17 17:09:10 +0530

    fixed bugs

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -3,8 +3,36 @@ import Navbar from "../shared/Navbar";
 import { Link } from "react-router-dom";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Label } from "../ui/label";
+import { useState } from "react";
+import { Input } from "../ui/input";
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        navigate("/");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+  };
   return (
     <div>
       <Navbar />
@@ -15,8 +43,8 @@ const Login = () => {
           </h2>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-4">
+        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={submitHandler} method="POST" className="space-y-4">
             <div>
               <label
                 htmlFor="email"
@@ -29,6 +57,8 @@ const Login = () => {
                   id="email"
                   name="email"
                   type="email"
+                  value={input.email}
+                  onChange={changeEventHandler}
                   required
                   autoComplete="email"
                   className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -58,6 +88,8 @@ const Login = () => {
                   id="password"
                   name="password"
                   type="password"
+                  value={input.password}
+                  onChange={changeEventHandler}
                   required
                   autoComplete="current-password"
                   className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -73,12 +105,26 @@ const Login = () => {
               </label>
               <RadioGroup className="flex gap-3">
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-one" id="option-one" />
-                  <Label htmlFor="option-one">Student</Label>
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="student"
+                    checked={input.role === "student"}
+                    onChange={changeEventHandler}
+                    id="student"
+                  />
+                  <Label htmlFor="student">Student</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-two" id="option-two" />
-                  <Label htmlFor="option-two">Recruter</Label>
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="recruter"
+                    checked={input.role === "recruter"}
+                    onChange={changeEventHandler}
+                    id="recruter"
+                  />
+                  <Label htmlFor="recruter">Recruter</Label>
                 </div>
               </RadioGroup>
             </div>
--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../shared/Navbar";
 import { Label } from "../ui/label";
-import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
-import { Link } from "react-router-dom";
+import { RadioGroup } from "../ui/radio-group";
+import { Link, useNavigate } from "react-router-dom";
+import { Input } from "../ui/input";
+import axios from "axios";
+import { USER_API_END_POINT } from "../utils/constant";
+import { toast } from "sonner";
 
 const Signup = () => {
+  const [input, setInput] = useState({
+    fullname: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+    role: "",
+    file: "",
+  });
+  const navigate = useNavigate();
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] });
+  };
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("fullname", input.fullname);
+    formData.append("email", input.email);
+    formData.append("phoneNumber", input.phoneNumber);
+    formData.append("password", input.password);
+    formData.append("role", input.role);
+    try {
+      const res = await axios.post(`${USER_API_END_POINT}/register`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        navigate("/login");
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
+    console.log(input);
+  };
   return (
     <div>
       <Navbar />
@@ -15,8 +59,8 @@ const Signup = () => {
           </h2>
         </div>
 
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-4">
+        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form method="POST" className="space-y-4" onSubmit={submitHandler}>
             <div>
               <label
                 htmlFor="email"
@@ -27,8 +71,10 @@ const Signup = () => {
               <div className="mt-2">
                 <input
                   id="fullName"
-                  name="fullName"
+                  name="fullname"
                   type="text"
+                  value={input.fullname}
+                  onChange={changeEventHandler}
                   required
                   autoComplete="name"
                   className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -48,6 +94,8 @@ const Signup = () => {
                   id="email"
                   name="email"
                   type="email"
+                  value={input.email}
+                  onChange={changeEventHandler}
                   required
                   autoComplete="email"
                   className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -67,6 +115,8 @@ const Signup = () => {
                   id="phoneNumber"
                   name="phoneNumber"
                   type="text"
+                  value={input.phoneNumber}
+                  onChange={changeEventHandler}
                   required
                   autoComplete="tel"
                   className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -88,9 +138,30 @@ const Signup = () => {
                   id="password"
                   name="password"
                   type="password"
+                  value={input.password}
+                  onChange={changeEventHandler}
                   required
                   autoComplete="current-password"
                   className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Profile Pic
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="profile"
+                  type="file"
+                  accept="image/*"
+                  onChange={changeFileHandler}
+                  className="cursor-pointer block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -98,17 +169,33 @@ const Signup = () => {
             <div className="radio-group">
               <label
                 htmlFor="role"
-                className="block text-sm font-medium leading-6 text-gray-900 mb-2"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Select your role
               </label>
               <RadioGroup className="flex gap-3">
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-one" id="option-one" />
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="student"
+                    checked={input.role === "student"}
+                    onChange={changeEventHandler}
+                    id="role_student"
+                    className="cursor-pointer"
+                  />
                   <Label htmlFor="option-one">Student</Label>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-two" id="option-two" />
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="recruter"
+                    checked={input.role === "recruter"}
+                    onChange={changeEventHandler}
+                    id="role_recruter"
+                    className="cursor-pointer"
+                  />
                   <Label htmlFor="option-two">Recruter</Label>
                 </div>
               </RadioGroup>
